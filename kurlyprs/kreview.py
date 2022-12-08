1d7e77541c74fb0b8d4a1ac7afec1ee370e5dd70
Choi <fckd0908@gmail.com>
2022-12-08 15:39:32 +0900

    1208_1539

--- kurlyprs/kreview.py
+++ kurlyprs/kreview.py
@@ -1,52 +1,63 @@
 import pandas as pd
 import re
 from kurlyprs.kdb import selBT,selSQL
-
 # SELECT a.id,substr(b.date,0,8) month,sale,title,count(rkey) cnt from beauty a 
 # left join breview b on a.pcode=b.pcode
 # where a.id=93 
 # group by a.id,substr(b.date,0,8)
 # order by b.date asc
-tbl='beauty a left join breview b on a.pcode=b.pcode'
-whr='where a.id=93 group by a.id,substr(b.date,0,8) order by b.date asc'
-cols='a.id,substr(b.date,0,8) month,sale,title,count(rkey) cnt'
-rows=selBT(tbl,whr,cols)
-rdf=pd.DataFrame(rows,columns=['id','month','sale','title','cnt'])
-rdf[['month','cnt']].plot()
+def drawPlot():
+    tbl='beauty a left join breview b on a.pcode=b.pcode'
+    whr='where a.id=93 group by a.id,substr(b.date,0,8) order by b.date asc'
+    cols='a.id,substr(b.date,0,8) month,sale,title,count(rkey) cnt'
+    rows=selBT(tbl,whr,cols)
+    rdf=pd.DataFrame(rows,columns=['id','month','sale','title','cnt'])
+    rdf[['month','cnt']].plot()
 
 
 
-msql=""" 
-SELECT substr(b.date,0,8) month,count(rkey) cnt from 
- beauty a left join breview b on a.pcode=b.pcode 
- where a.id={} group by substr(b.date,0,8) order by b.date asc
-"""
+
+def monthBeauty(pcode):
+    msql=""" 
+        SELECT a.pcode,substr(b.date,0,8) month,count(rkey) cnt from 
+        beauty a left join breview b on a.pcode=b.pcode   
+        where a.pcode={} group by substr(b.date,0,8) order by b.date asc
+        """.format(pcode)
+    
+    rows=selSQL(msql)
+    return rows
+
+def monthReview(pcode):
+    msql=""" 
+        SELECT substr(date,0,8) month,review from 
+        breview 
+        where pcode={} order by date asc
+        """.format(pcode)
+    print(msql)
+    rows=selSQL(msql)
+    return rows
 
 def getTopReview(cnt=10):
     sql=""" 
-    SELECT a.pcode, a.title, count(rkey) cnt from beauty a 
+    SELECT a.pcode,a.title,count(rkey) cnt from beauty a 
     left join breview b on a.pcode=b.pcode 
     group by a.id
     order by cnt desc
     limit {}
     """.format(cnt)
     rows=selSQL(sql)
-    
     return rows
 
 def getReview(pcode=0):
-    sql="SELECT review from breview WHERE pcode={}".format(pcode)
-    
+    sql=" SELECT review from breview where pcode={}".format(pcode)
+    print(sql)
     rows=selSQL(sql)
-    
     return rows
 
 def studyReview():
     df=pd.DataFrame()
-
     for r in rows:
         rows=selSQL(msql.format(r[0]))
         mdf=pd.DataFrame(rows,columns=['id',str(r[0])]).set_index('id')
         df=pd.merge(df,mdf,how='outer',left_index=True,right_index=True)
-    
     df.iloc[:-1,:].fillna(0).plot()
--- mybow.py
+++ mybow.py
@@ -9,10 +9,9 @@ import seaborn as sns
 import pandas as pd
 import re
 from kurlyprs.kdb import selBT,selSQL
-from kurlyprs.kreview import getReview, getTopReview
-from konlpy.tag import Kkma
-
+from kurlyprs.kreview import getReview,getTopReview
 #%%
+from konlpy.tag import Kkma
 
 # 데이터 준비
 rows=selBT("breview",'limit 1,2','review')
@@ -32,7 +31,7 @@ def makeSentence(senten):
         if(po[1] in pocate):
             wordlist.append(po[0])
             sentence=' '.join(wordlist)
-    return sentence, wordlist
+    return sentence,wordlist
 
 def getCorpus(rows=[]):
     corpus=[]
@@ -48,42 +47,53 @@ def getCorpus(rows=[]):
 cl,wl=getCorpus(rows)
 print(cl)
 print(wl)
-
 # %%
+# 많이 나온 단어가 주된 단어다
 from sklearn.feature_extraction.text import CountVectorizer
-
-cv = CountVectorizer(stop_words=['록시땅','컬리'], ngram_range=(1,1), min_df=0)
+cv=CountVectorizer(stop_words=['록시땅','컬리'],ngram_range=(2,3),tokenizer=lambda txt: txt.split())
 X = cv.fit_transform(cl)
-names = cv.get_feature_names_out()
-cbow = X.toarray()
-print("names:", names)
-# print('단어 인덱스:', cv.vocabulary_)
-print('cbow:', cbow)
-
+names=cv.get_feature_names_out()
+cbow=X.toarray()
+print('names:',names)
+print('단어 인덱스:',cv.vocabulary_)
+print('cbow:',cbow)
 # %%
-# TF-IDF
-# 모든 문장에서 빈번하게 나오는 단어는 그 가중치 값이 떨어짐.
-# ngram으로 묶어서 "축구팀 못했다", "축구팀 잘했다" 와 같이 나오면 훨씬 낫다
+# TF_IDF
+cl=['i am the boy in the house','i am the girl in the house']
 from sklearn.feature_extraction.text import TfidfVectorizer
-tv = TfidfVectorizer(stop_words=['록시땅','컬리'], ngram_range=(1,1), min_df=0)
+tv=TfidfVectorizer(stop_words=['록시땅','컬리'],ngram_range=(1,1),tokenizer=lambda txt: txt.split())
 X = tv.fit_transform(cl)
 names=tv.get_feature_names_out()
-tbow = X.toarray()
-
-print('names:',names)
-# print('단어 인덱스:', tv.vocabulary_)
-print('tbow:', tbow)
-
+tbow=X.toarray()
 # %%
-# LDA(잠재 디리클레 할당)
+print('names:',names)
+print('단어 인덱스:',tv.vocabulary_)
+print('cbow:',tbow)
+# %%
 
-#%%
+cl=['i am the boy in the house','i am the girl in the house']
+
+tv=TfidfVectorizer(stop_words=['록시땅','컬리'],ngram_range=(1,1),tokenizer=lambda txt: txt.split())
+X = tv.fit_transform(cl)
+names=tv.get_feature_names_out()
+tbow=X.toarray()
+print('names:',names)
+
+print('cbow:',tbow)
+
+cv=CountVectorizer(stop_words=['록시땅','컬리'],ngram_range=(1,1),tokenizer=lambda txt: txt.split())
+X = cv.fit_transform(cl)
+names=cv.get_feature_names_out()
+cbow=X.toarray()
+print('names:',names)
+
+print('cbow:',cbow)
+# %%
 import gensim
-#%%
-trows = getTopReview(1)
+# %%
+trows=getTopReview(2)
 for tr in trows:
     print(tr)
-    revs = getReview(tr[0])
-    
+    revs=getReview(tr[0])
 revs
 # %%
